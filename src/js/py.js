bc528e3a5889319aa1a6449766fafa6b5fbffa4f
Ryan Berdeen <ryan@echonest.com>
2010-03-21 15:08:30 -0400

    fix exception handling in pyeval

--- src/js/py.js
+++ src/js/py.js
@@ -1,7 +1,10 @@
-window.__pyevalresult = null;
-
 function pyeval(string) {
+    window.__pyevalresult = null;
+    window.__pyevalexception = null;
     window.__pyevalarg = string;
     window.__pyevalfn(null, null);
+    if (window.__pyevalexception) {
+        throw window.__pyevalexception;
+    }
     return window.__pyevalresult;
 }
--- src/py/js.py
+++ src/py/js.py
@@ -1,12 +1,25 @@
 import System
 
-#from System import Array
-#window.Invoke("test", Array[object]([]))
+from System import Array
+
+def log(m):
+    window.console.Invoke('log', Array[object]([m]))
+
+EXCLUDE_TYPES = set(['module', 'namespace#', 'function', 'classobj'])
 
 def pyeval(s, e):
-    g = {}
-    exec window.__pyevalarg in g
-    g.pop('__builtins__')
-    window.__pyevalresult = repr(g)
+    try:
+        g = {}
+        exec window.__pyevalarg in g
+        g.pop('__builtins__')
+        result = {}
+        for k in g.iterkeys():
+            v = g[k]
+            t = type(v).__name__
+            if t not in EXCLUDE_TYPES:
+                result[k] = v
+        window.__pyevalresult = repr(result)
+    except Exception as ex:
+        window.__pyevalexception = repr(ex)
 
 window.__pyevalfn = System.EventHandler[System.Windows.Browser.HtmlEventArgs](pyeval)
